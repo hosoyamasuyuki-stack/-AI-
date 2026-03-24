ecba87de365214b3441c580db2facb3821ce7f10
hosoyamasuyuki-stack <hosoya.masuyuki@gmail.com>
2026-03-24 11:39:04 +0900

    Create record_changelog.py

---
+++ record_changelog.py
@@ -0,0 +1,35 @@
+# record_changelog.py
+# GitHub Actionsから呼ばれる。コミット情報をHandover_ChangeLogに自動追記。
+# 環境変数: GOOGLE_CREDENTIALS / SPREADSHEET_ID / COMMIT_MSG / COMMIT_SHA / CHANGED_FILES
+
+import os, json, gspread
+from google.oauth2.service_account import Credentials
+from datetime import datetime
+
+SPREADSHEET_ID = os.environ.get('SPREADSHEET_ID',
+    '1GtlVhGcPjMU0pJWsijwnmTe1rFJXAGvkaJFjav9gGcE')
+creds_dict = json.loads(os.environ.get('GOOGLE_CREDENTIALS', '{}'))
+scopes = ['https://www.googleapis.com/auth/spreadsheets',
+          'https://www.googleapis.com/auth/drive']
+creds = Credentials.from_service_account_info(creds_dict, scopes=scopes)
+gc = gspread.authorize(creds)
+ss = gc.open_by_key(SPREADSHEET_ID)
+
+commit_msg    = os.environ.get('COMMIT_MSG', '(no message)')
+commit_sha    = os.environ.get('COMMIT_SHA', '')[:7]
+changed_files = os.environ.get('CHANGED_FILES', '').replace('\n', ' / ')
+now = datetime.now().strftime('%Y/%m/%d %H:%M')
+
+SHEET = 'Handover_ChangeLog'
+try:
+    ws = ss.worksheet(SHEET)
+except Exception:
+    ws = ss.add_worksheet(title=SHEET, rows=500, cols=5)
+    ws.update('A1', [['\u65e5\u6642', 'SHA', '\u30b3\u30df\u30c3\u30c8\u30e1\u30c3\u30bb\u30fc\u30b8', '\u5909\u66f4\u30d5\u30a1\u30a4\u30eb', '\u5099\u8003']])
+    print(f'Created: {SHEET}')
+
+row = [now, commit_sha, commit_msg, changed_files, '']
+all_vals = ws.get_all_values()
+next_row = len(all_vals) + 1
+ws.update(f'A{next_row}', [row])
+print(f'OK: ChangeLog\u8ffd\u8a18 ({now} / {commit_sha} / {commit_msg})')
